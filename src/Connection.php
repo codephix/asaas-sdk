7170f506b234e916897c858e95216c4bece6abbe
Max Alex - Codephix <contato@codephix.com>
2024-07-18 19:00:50 -0300

    Correção link URL Api

--- src/Connection.php
+++ src/Connection.php
@@ -23,7 +23,7 @@ class Connection
             die('Tipo de homologação invalida');
         }
         $this->api_key = $token;
-        $this->base_url = "https://" . (($this->api_status) ? 'sandbox' : 'www');
+        $this->base_url = "https://" . (($this->api_status) ? 'sandbox.asaas.com/api' : 'api.asaas.com');
 
         return $this;
     }
@@ -32,7 +32,7 @@ class Connection
     public function get($url, $option = false, $custom = false)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->base_url . '.asaas.com/api/v3' . $url . $option);
+        curl_setopt($ch, CURLOPT_URL, $this->base_url . '/v3' . $url . $option);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
@@ -68,7 +68,7 @@ class Connection
         $params = json_encode($params);
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, $this->base_url . '.asaas.com/api/v3' . $url);
+        curl_setopt($ch, CURLOPT_URL, $this->base_url . '/v3' . $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
